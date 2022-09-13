fbae46de087d173f963a6af4e4172ff78a997eec
sunxingyu <sunxingyu@thunisoft.com>
2022-09-13 11:40:47 +0800

    编程实现 rtp vp8流收取与渲染

--- demo1/appsrc_rtp_vp8.cpp
+++ demo1/appsrc_rtp_vp8.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <atomic>
 #include "appsrc.h"
+#include <exception>
+#include <iostream>
+#include "gst/gstregistry.h"
 using namespace std;
 
 static GMainLoop* gstreamer_receive_main_loop = NULL;
@@ -69,25 +72,95 @@ static void mainLoop() {
 	g_main_loop_run(gstreamer_receive_main_loop);
 }
 
-int tutorial_8(atomic<bool> * flag) {
-	GstBus* bus;
+static void cb_new_pad(GstElement* element, GstPad* pad, gpointer data)
+{
+	gchar* name;
+	GstElement* other = (GstElement*)data;
 
+	name = gst_pad_get_name(pad);
+	g_print("A new pad %s was created for %s\n", name, gst_element_get_name(element));
+	g_free(name);
+
+	g_print("element %s will be linked to %s\n",
+		gst_element_get_name(element),
+		gst_element_get_name(other));
+	gst_element_link(element, other);
+}
+
+int tutorial_8(atomic<bool>* flag) {
+	GstBus* bus;
+	GstPad* tee_vd1_pad, * tee_vd2_pad;
+	GstPad* queue_vd1_pad, * queue_vd2_pad;
+	GstElement* appsrc;
 	/* Initialize GStreamer */
 	gst_init(NULL, NULL);
+	GstElement* decodebin = gst_element_factory_make("vp8dec", "decode");
 
-	pipeline =
+	appsrc = gst_element_factory_make("appsrc", "src");
+	GstElement* tee = gst_element_factory_make("tee", "tee1");
+	GstElement* rtpvp8depay = gst_element_factory_make("rtpvp8depay", "depay");
+	GstElement* capsFilter = gst_element_factory_make("capsfilter", "cf");
+	GstElement* videoConvert = gst_element_factory_make("videoconvert", "vc");
+
+	GstElement* videoSink1 = gst_element_factory_make("autovideosink", "vd1");
+	pipeline = gst_pipeline_new("test-pipeline");
+	g_object_set(G_OBJECT(appsrc), "format", GST_FORMAT_TIME, NULL);
+	g_object_set(G_OBJECT(appsrc), "is-live", TRUE, NULL);
+	//g_object_set(G_OBJECT(appsrc), "do-timestamp", TRUE, NULL);
+
+	/*GstCaps* caps1 = gst_caps_from_string("application/x-rtp, encoding-name=VP8-DRAFT-IETF-01, clock-rate=90000, media=video");
+	g_object_set(G_OBJECT(appsrc), "caps", caps1, NULL);*/
+	GstCaps* caps1 = gst_caps_from_string("application/x-rtp, payload=96, encoding-name=VP8-DRAFT-IETF-01");
+	g_object_set(G_OBJECT(capsFilter), "caps", caps1, NULL);
+
+
+	/* Link all elements that can be automatically linked because they have "Always" pads */
+	gst_bin_add_many(GST_BIN(pipeline), appsrc, rtpvp8depay, decodebin, videoConvert, capsFilter, videoSink1, NULL);
+	if (gst_element_link_many(appsrc, capsFilter, rtpvp8depay, decodebin, videoConvert, videoSink1, NULL) != TRUE) {
+		g_printerr("Elements could not be linked.\n");
+		gst_object_unref(pipeline);
+		return -1;
+	}
+
+
+	/*GstPad *rtpvp8depaySinkPad = gst_element_get_static_pad(rtpvp8depay, "sink");
+	GstCaps* caps = gst_caps_from_string("application/x-rtp, payload=96, encoding-name=VP8-DRAFT-IETF-01 clock-rate=90000, media=video");
+	gst_pad_set_caps(rtpvp8depaySinkPad, caps);*/
+
+	/*gst_element_link_pads(appsrc, "src", rtpvp8depay, "sink");
+	gst_element_link_pads(rtpvp8depay, "src", decodebin, "sink");*/
+
+
+	/*pipeline =
 		gst_parse_launch
-		("appsrc format=time is-live=true do-timestamp=true name=src ! application/x-rtp, payload=96, encoding-name=VP8-DRAFT-IETF-01 ! rtpvp8depay ! decodebin ! autovideosink", NULL);
+		("appsrc format=time is-live=true do-timestamp=true name=src ! application/x-rtp, payload=96, encoding-name=VP8-DRAFT-IETF-01 ! rtpvp8depay ! decodebin ! tee name=tee1", NULL); */
+
+		//tee_vd1_pad = gst_element_request_pad_simple(tee, "src_%u");
+		//g_print("Obtained request pad %s for audio branch.\n", gst_pad_get_name(tee_vd1_pad));
+		//queue_vd1_pad = gst_element_get_static_pad(videoSink1, "sink");
+		//tee_vd2_pad = gst_element_request_pad_simple(tee, "src_%u");
+		//g_print("Obtained request pad %s for video branch.\n", gst_pad_get_name(tee_vd2_pad));
+		//queue_vd2_pad = gst_element_get_static_pad(videoSink2, "sink");
+		//if (gst_pad_link(tee_vd1_pad, queue_vd1_pad) != GST_PAD_LINK_OK ||
+		//	gst_pad_link(tee_vd2_pad, queue_vd2_pad) != GST_PAD_LINK_OK) {
+		//	g_printerr("Tee could not be linked.\n");
+		//	gst_object_unref(pipeline);
+		//	return -1;
+		//}
+		//gst_object_unref(queue_vd1_pad);
+		//gst_object_unref(queue_vd2_pad);
+
+		//g_signal_connect(decodebin, "pad-added", G_CALLBACK(cb_new_pad), videoSink1);
 
 	bus = gst_element_get_bus(pipeline);
-	gst_bus_add_watch(bus, (GstBusFunc)handleMessage, mainLoop);
+	gst_bus_add_watch(bus, (GstBusFunc)handleMessage, gstreamer_receive_main_loop);
 	/*gst_bus_add_signal_watch(bus);
 	g_signal_connect(G_OBJECT(bus), "message::error", (GCallback)error_cb, gstreamer_receive_main_loop);*/
 	gst_object_unref(bus);
-	flag->store(true);
 	/* Start playing */
 	GstStateChangeReturn stateChangeResult = gst_element_set_state(pipeline, GST_STATE_PLAYING);
-	//printf("%d", stateChangeResult);
+	printf("%d", stateChangeResult);
+	flag->store(true);
 
 	mainLoop();
 
